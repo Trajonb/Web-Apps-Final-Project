0f5cad0a4aa42d3ca08066a45c8f11c82614d6ef
Trajonb <80763199+Trajonb@users.noreply.github.com>
2021-03-27 07:46:29 -0400

    A lot of changes

---
+++ fin_app.js
@@ -0,0 +1,15 @@
+$(".comment-input button").on("click", function (event) { 
+    $(".bg-modal").css("display", "flex");
+}); 
+
+$(".close").on("click", function (event) { 
+    $(".bg-modal").css("display", "none");
+}); 
+
+$(".button2").on("click", function (event) { 
+    $(".bg-modal").css("display", "none");
+
+}); 
+
+
+  

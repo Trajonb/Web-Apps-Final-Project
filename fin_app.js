a7fde8d774cfa30df1858219726da2006f77a4d8
Trajonb <80763199+Trajonb@users.noreply.github.com>
2021-04-07 10:26:07 -0400

    New Page

--- fin_app.js
+++ fin_app.js
@@ -7,17 +7,7 @@ $(".close").on("click", function (event) {
 }); 
 
 $(".button2").on("click", function (event) { 
-    var txtFile = "C:\subscribers.txt";
-    var file = new File(txtFile); 
-    const fs = require('fs');
-    var input = $(".subfile input").val(); //get value of input and makes it >
-    if (input == "") { //if the input box is empty
-         window.alert("Empty!"); //displays window alert  notfying that >
-    } else {
-        file.open("w"); // open file with write access
-        file.writeln(input);
-        file.close();
-    }
+    
     $(".bg-modal").css("display", "none");
 
 }); 
